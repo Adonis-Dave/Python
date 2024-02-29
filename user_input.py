1f977dbc373c3fc5f402d2d44e4ff3ded47ef078
David Denis <emilydean656@gmail.com>
2024-02-29 13:46:40 +0300

    First Python Commit

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = input("Please enter your age: ")
+location = input("Where do you live: ")
+
+u = 'Hello ' + name + " you are " + age + " years old and live in " + location
+
+print(u)
